ed67fc25d0833b484c6b95aa1b1435b30a019bc7
hedgehog125 <16543008+hedgehog125@users.noreply.github.com>
2021-12-29 02:06:08 +0000

    I'm getting a backup of this. Also I started making an assembler

---
+++ Bagembly/assets/scripts/main.js
@@ -0,0 +1,297 @@
+/*
+TODO
+*/
+
+let game = (_ => {
+    let memoryAddressLength = instructionSet => instructionSet.memoryAddressLength;
+    let accAddressLength = instructionSet => instructionSet.accumulatorAddressLength;
+
+    return Bagel.init({
+        id: "BagnaryCodeEmulator",
+        state: "main",
+        vars: {
+            execution: {
+                powers: [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768],
+                binaryToDenary: binary => {
+                    let total = 0;
+                    for (let i in binary) {
+                        if (binary[i] == "1") {
+                            total += game.vars.execution.powers[(binary.length - 1) - i];
+                        }
+                    }
+                    return total;
+                },
+                denaryToBinary: (denary, length) => {
+                    let binary = "";
+
+                    let powers = game.vars.execution.powers;
+                    let i = length - 1;
+                    while (i != -1) {
+                        if (denary >= powers[i]) {
+                            denary -= powers[i];
+                            binary += "1";
+                            if (denary == 0) break;
+                        }
+                        else {
+                            binary += "0";
+                        }
+                        i--;
+                    }
+                    return binary;
+                },
+                instructions: {
+                    stop: {
+                        arguments: []
+                    },
+                    loadAccBit: {
+                        arguments: [memoryAddressLength, accAddressLength]
+                    },
+                    jumpIf: {
+                        arguments: [memoryAddressLength, accAddressLength]
+                    },
+                    saveAccBit: {
+                        arguments: [memoryAddressLength, accAddressLength]
+                    },
+
+                    // Compile constants
+                    "#Pointer": {
+                        arguments: [-1]
+                    },
+                    "#InstructionSet": { // Doesn't do anything here
+                        arguments: [-1]
+                    }
+                },
+                instructionSets: {
+                    "1.0": {
+                        instructionCodeLength: 4,
+                        accumulatorAddressLength: 3,
+                        memoryAddressLength: 8,
+                        used: [
+                            "stop",
+                            "loadAccBit",
+                            "jumpIf",
+                            "saveAccBit",
+
+                            "#Pointer",
+                            "#InstructionSet"
+                        ]
+                    },
+                    "1.1": {
+                        instructionCodeLength: 2,
+                        accumulatorAddressLength: 4,
+                        memoryAddressLength: 16,
+                        used: [
+                            "stop",
+                            "loadAccBit",
+                            "jumpIf",
+                            "saveAccBit",
+
+                            "#Pointer",
+                            "#InstructionSet"
+                        ]
+                    }
+                },
+
+                assembleProgram: program => {
+                    let subFunctions = game.vars.execution;
+                    program = subFunctions.parseProgram(program);
+                    let assembly = subFunctions.assembleCommands(program);
+                    assembly = subFunctions.compileConstants(program, assembly);
+
+                    debugger
+                    return assembly;
+                },
+                parseProgram: program => {
+                    let parsed = {};
+                    // Find the instruction set specified in the file
+                    let keyword = "#InstructionSet ";
+                    let index = program.indexOf(keyword);
+                    if (index == -1) {
+                        alert("No instruction set specified in program. Specify one by putting \"#InstructionSet <name>\" at the top of the program file.");
+                        return;
+                    }
+                    else {
+                        let end = program.indexOf("\n", index + keyword.length);
+                        let hashClose = program.indexOf("#", index + keyword.length);
+                        if (hashClose != -1) {
+                            end = hashClose;
+                        }
+
+                        let name = program.slice(index + keyword.length, end);
+                        parsed.instructionSetName = name;
+                        parsed.instructionSet = game.vars.execution.instructionSets[name];
+                        if (parsed.instructionSet == null) {
+                            alert("Invalid instruction set of " + JSON.stringify(name) + ".");
+                            return;
+                        }
+                    }
+
+                    let lines = program.split("\n");
+                    let commands = [];
+                    let lineNumbers = [];
+                    let inMultiComment = false;
+                    for (let i in lines) {
+                        let line = lines[i];
+                        let inCompileCommand = false;
+                        let command = [];
+
+                        // Skip over whitespace
+                        let c = 0;
+                        while (line[c] == " " || line[c] == "   ") {
+                            c++;
+                        }
+
+                        let subCommand = "";
+                        while (c < line.length) {
+                            let nextTwo = line[c] + line[c + 1];
+                            if (inMultiComment) {
+                                if (nextTwo == "*/") {
+                                    inMultiComment = false;
+                                    c += 2;
+                                }
+                            }
+                            else {
+                                if (nextTwo == "/*") {
+                                    inMultiComment = true;
+                                    c += 2;
+                                }
+                            }
+
+                            if (inMultiComment || nextTwo == "//") {
+                                if (nextTwo == "//") {
+                                    break;
+                                }
+                                else {
+                                    c++;
+                                }
+                                continue;
+                            }
+                            if (line[c] == "#") {
+                                if (! inCompileCommand) {
+                                    command.push("#");
+                                }
+                                inCompileCommand = ! inCompileCommand;
+                                c++;
+                                continue;
+                            }
+
+                            let endOfLine = c + 1 >= line.length;
+                            if (line[c] == " " || line[c] == "  " || endOfLine) {
+                                if (endOfLine && c + 1 == line.length) {
+                                    subCommand += line[c];
+                                }
+                                if (subCommand.length != 0) {
+                                    command.push(subCommand);
+                                    subCommand = "";
+                                }
+                            }
+                            else {
+                                subCommand += line[c];
+                            }
+                            c++;
+                        }
+
+                        if (command.length != 0) {
+                            commands.push(command);
+                            lineNumbers.push(parseInt(i));
+                        }
+                    }
+                    parsed.commands = commands;
+                    parsed.lineNumbers = lineNumbers;
+                    return parsed;
+                },
+                assembleCommands: program => { // Also validates
+                    let instructionNames = program.instructionSet.used;
+                    for (let i in program.commands) {
+                        let command = program.commands[i];
+                        let instructionName = command[0];
+
+                        let compileConstant = instructionName == "#";
+                        if (compileConstant) { // Compile constants aren't dealt with yet but they are validated
+                            instructionName = "#" + command[1]; // It's in 2 parts
+                        }
+                        if (! instructionNames.includes(instructionName)) {
+                            alert("Invalid instruction " + JSON.stringify(instructionName) + " on line " + (program.lineNumbers[i] + 1) + ".");
+                            return;
+                        }
+                        if (compileConstant) continue;
+                    }
+                },
+                compileConstants: (program, assembly) => { // Calculate the values of the compile constants
+
+                }
+            }
+        },
+        game: {
+            assets: {
+                imgs: [
+                    {
+                        id: "Upload",
+                        src: "../assets/imgs/upload.png"
+                    }
+                ]
+            },
+            plugins: [
+                {
+                    src: "../assets/plugins/gui.js"
+                }
+            ],
+            sprites: [
+                {
+                    type: "GUI",
+                    id: "Menu",
+                    submenu: "upload",
+                    submenus: {
+                        upload: {
+                            hoverText: {
+                                color: "white"
+                            },
+                            elements: [
+                                {
+                                    type: "button",
+                                    onClick: _ => {
+                                        Bagel.upload(url => {
+                                            let typeAndEncoding = url.split(":")[1].split(",")[0];
+                                            let type = typeAndEncoding.split(";")[0];
+                                            let contents = url.split(",");
+                                            contents.splice(0, 1);
+                                            contents = contents.join(",");
+
+                                            if (type != "text/plain") {
+                                                alert("Wrong file type. It should be text/plain.");
+                                                return;
+                                            }
+
+                                            if (typeAndEncoding.includes("base64")) {
+                                                contents = atob(contents);
+                                            }
+                                            let execution = game.vars.execution;
+                                            Bagel.download(
+                                                execution.assembleProgram(contents),
+                                                "program.txt"
+                                            );
+                                        });
+                                        game.input.mouse.down = false;
+                                    },
+                                    onHover: "Upload a program to assemble",
+                                    color: "yellow",
+                                    icon: "Upload",
+                                    iconSize: 0.6,
+                                    size: 150
+                                }
+                            ]
+                        }
+                    },
+                    stateToActivate: "main"
+                }
+            ]
+        },
+        width: 800,
+        height: 450,
+        config: {
+            display: {
+                backgroundColor: "#202020"
+            }
+        }
+    });
+})();
